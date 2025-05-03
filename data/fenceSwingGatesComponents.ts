da400dc3c19e2eb792855339b79b808b3441c266
aditya-obj <adityaakumar.in@gmail.com>
2025-05-03 11:52:53 +0530

    added data

--- data/fenceSwingGatesComponents.ts
+++ data/fenceSwingGatesComponents.ts
@@ -33,19 +33,35 @@ export const componentSpecifications = {
   "B. GATE FRAME": {
     description: "No description available.",
     specifications: [
-      { label: "", value: "" },
+      { label: "Top, bottom, side and brace section", value: "50x50x3.2mm/80x80x3.2mm Mild steel Square Hollow section (SHS), conforming to IS 4923-2017 (Yst - 240MPa)." },,
+      { label: "Size", value: "2505mm x 2565mm / 2490mm x 2665mm." },
+      { label: "Qty per Gate", value: "2 Nos." },
+      { label: "Base Coating", value: "Hot dip galvanized coating, done as per IS:2629, shall have minimum mass of 360 GSM, as determined as per IS:6745. The coating conforms to IS 4759." },
+      { label: "Thermoplastic Coating", value: "The galvanized frame shall subsequently have thermoplastic coating after welding with a thickness of minimum 250 μm for C4 / 400 μm for C5." },
+      { label: "Color", value: "Green (RAL 6005) Coating Standard: IS 13871." },
     ]
   },
   "C. WICKET GATE FRAME": {
     description: "No description available.",
     specifications: [
-      { label: "", value: "" },
+      { label: "Section", value: "40x40x3.2mm Mild steel Square Hollow section (SHS), conforming to IS 4923-2017 (Yst – 240MPa)." },
+      { label: "Qty per Gate", value: "1 Nos." },
+      { label: "Base Coating", value: "Hot dip galvanized coating, done as per IS:2629, shall have minimum mass of 360 GSM, as determined as per IS:6745. The coating shall conform to IS 4759." },
+      { label: "Thermoplastic Coating", value: "The galvanized posts shall subsequently be thermoplastic coated, after welding, with a thickness of minimum 250 μm for C4 / 400 μm for C5." },
+      { label: "Color", value: "Green (RAL 6005) Coating Standard: IS 13871." },
     ]
   },
   "D. WELD MESH PANEL FOR GATE INFILL": {
     description: "No description available.",
     specifications: [
-      { label: "", value: "" },
+      { label: "Dimensions & Standards of Panel", value: "Weld mesh panel for fence shall meet the standards of EN 10223-7." },
+      { label: "Mesh Size", value: "76.2 mm ± 3 mm X 12.7 mm ± 2 mm (W X H.)" },
+      { label: "Weld Shear Strength", value: "The average weld shear strength of 4 welds taken at random shall not be less than 50% of the breaking strength of the smallest wire (in this case the vertical wire)." },
+      { label: "Mesh Wire", value: "Galvanized MS wire conforming to requirements of IS 280-2006 as well as IS:7887. Wire diameter 4.0 mm (± 0.05mm)." },
+      { label: "Tensile Strength of wire", value: "Min 550 N/sq.mm." },
+      { label: "Galvanizing of wire", value: "Hot dip galvanized coating, done as per IS:2629, shall have minimum mass of 100 GSM, as determined as per IS:6745. The coating shall conform to IS 4826." },
+      { label: "Thermoplastic coating", value: "The Panel shall be Thermoplastic powder coated after fabrication on galvanized base with a thickness of minimum 250 μm for C4 / 400 μm for C5." },
+      { label: "Color", value: "Green (RAL 6005) Coating Standard: IS 13871." },
     ]
   },
   "E. COIL SUPPORT FRAME": {
@@ -74,13 +90,37 @@ export const componentSpecifications = {
   "F. PTCC – 850 MM DIAMETER/16 LOOPS/3.5MM CORE WIRE": {
     description: "No description available.",
     specifications: [
-      { label: "", value: "" },
+      { label: "Coil Diameter", value: "850mm +/- 50mm." },
+      { label: "Steel tape", value: "Material SS304 for C4/ SS 316 for C5 as per IS 6911/1992." },
+      { label: "Blade Thickness", value: "0.45 mm +/- 0.02mm." },
+      { label: "Core wire diameter", value: "3.5mm +/- 0.05 mm as per IS 6528\nCore wire: Material SS304 for C4/ SS 316 for C5." },
+      { label: "Tensile Strength of wire", value: "1400-1500 N/sq.mm." },
+      { label: "Blade Profile", value: "Tear Blade (TBBT)." },
+      { label: "Blade Length", value: "30mm." },
+      { label: "Blade Width", value: "16mm ± 0.05 mm." },
+      { label: "Centre to Centre Distance Between blades", value: "50mm." },
+      { label: "Material of clip", value: "SS 304 for C4 / SS 316 for C5." },
+      { label: "Number of loops", value: "16 Loops." },
+      { label: "Weight per coil", value: "5.74 kg (+/- 5%)." },
+      { label: "Quantity", value: "2 coils per Gate." },
     ]
   },
   "G. FLAT WRAP (PTCC): 610 MM DIAMETER/10 LOOPS/3.50 MM CORE WIRE": {
     description: "No description available.",
     specifications: [
-      { label: "", value: "" },
+      { label: "Flat wrap Diameter", value: "850mm +/- 50mm." },
+      { label: "Steel tape", value: "Material SS304 for C4/ SS 316 for C5 as per IS 6911/1992." },
+      { label: "Blade Thickness", value: "0.45 mm +/- 0.02mm." },
+      { label: "Core wire diameter", value: "3.50mm +/- 0.05 mm as per IS 6528 Core wire." },
+      { label: "Core wire", value: "SS304 for C4/ SS 316 for C5 as per IS 6911/1992." },
+      { label: "Tensile Strength of wire", value: "1400-1500 N/sq.mm." },
+      { label: "Blade Profile", value: "Tear Blade (TBBT)." },
+      { label: "Blade Length", value: "30mm." },
+      { label: "Blade Width", value: "16mm ± 0.05 mm." },
+      { label: "Centre to Centre Distance Between blades", value: "50mm Material of clip: Material." },
+      { label: "Material", value: "SS 304 for C4 / SS 316 for C5." },
+      { label: "Number of loops / coil", value: "10 Nos." },
+      { label: "Qty per Gate", value: "2 coil." },
     ]
   },
 };
